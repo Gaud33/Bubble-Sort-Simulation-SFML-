7dbe91275c25b611f1e228cf9f063c6bcec675f6
Gaud33 <kafleegaurav@gmail.com>
2023-07-13 18:55:00 +0545

    update 2

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -8,14 +8,27 @@ int main()
 
     sf::Event ev;
     
-
     sf::RectangleShape arr[ARR_SIZE];
 
     sf::Color tempColor;
     float tempPos;
     bool sorted = false;
-    int outerCounter = 0;
+
+    // for completion text
+    sf::Font font;
+    sf::Text completionText;
+    font.loadFromFile("ArialTh.ttf" );
+    completionText.setFont(font);
+    completionText.setCharacterSize(40);
+    completionText.setPosition(sf::Vector2f(300, 50));
+    completionText.setFillColor(sf::Color::Black);
+    completionText.setString("Sorting Complete!");
+    // pass counter
+    int outerCounter = 0; 
+
+    // iteration counter
     int j = 0;
+
     //randomize height
     time_t t;
     srand((unsigned)time(&t));
@@ -26,9 +39,9 @@ int main()
     sf::Vector2f pos = sf::Vector2f(50, 450);
     int R = 50, G = 240, B = 170;
     for (int i = 0; i < ARR_SIZE; i++) {
-        arr[i].setFillColor(sf::Color(R+= 5,  G, B += 5 ));
+        arr[i].setFillColor(sf::Color::White );
         arr[i].setPosition(pos.x += 13, pos.y);
-        arr[i].setSize(sf::Vector2f(10, rand() % (200 - 20 - 1) + 20));
+        arr[i].setSize(sf::Vector2f(10, rand() % (300 - 20 - 1) + 20));
     }
     
 
@@ -49,7 +62,7 @@ int main()
         elapsed = clock.getElapsedTime();
 
         // sort alogrithm runs only once every 0.05s for visibility
-        if (elapsed.asSeconds() >= 0.05f)
+        if (elapsed.asSeconds() >= 0.015f)
         {
                     // sort the array using bubble sort
                     if (arr[j].getSize().y < arr[j + 1].getSize().y) {
@@ -57,12 +70,15 @@ int main()
                         tempColor = arr[j].getFillColor();
 
                         arr[j].setSize(sf::Vector2f(10, arr[j + 1].getSize().y));
-                        arr[j].setFillColor(arr[j + 1].getFillColor());
+                       // arr[j].setFillColor(arr[j + 1].getFillColor());
 
                         arr[j + 1].setSize(sf::Vector2f(10, tempPos));
-                        arr[j + 1].setFillColor(tempColor);
-   
+                        //arr[j + 1].setFillColor(tempColor);
+                        
+                        
                     }
+                    arr[j].setFillColor(sf::Color::White);
+                    arr[j + 1].setFillColor(sf::Color::White);
                     j++; // increment inner loop
 
                     if (j >= ARR_SIZE - outerCounter - 1) {
@@ -70,13 +86,13 @@ int main()
                         outerCounter++; // increment pass
                         j = 0;
                     }
+
+                    if (outerCounter == ARR_SIZE - 2) sorted = true;
                     
                
             skip:
             clock.restart();
         }
-
-        
 
         window.clear();
 
@@ -84,9 +100,22 @@ int main()
         for (int i = 0; i < ARR_SIZE; i++) {
 
             arr[i].setOrigin(0, arr[i].getSize().y);
+           
             window.draw(arr[i]);
+            
+
+            if (!sorted) {
+                arr[j].setFillColor(sf::Color::Red);
+                arr[j + 1].setFillColor(sf::Color::Red);
+            }
+           
         }
 
+        window.draw(completionText);
+
+        if (sorted) {
+            completionText.setFillColor(sf::Color::White);
+        }
         window.display();
        
     }
